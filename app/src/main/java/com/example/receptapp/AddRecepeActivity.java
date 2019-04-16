1bd623d2a57304f08626d455696d3f9f426e8daf
sara-sall <sarasall092@gmail.com>
2019-04-16 12:52:38 +0200

    Layout to add recepe, adding extra textInput fields

--- app/src/main/java/com/example/receptapp/AddRecepeActivity.java
+++ app/src/main/java/com/example/receptapp/AddRecepeActivity.java
@@ -1,13 +1,21 @@
 package com.example.receptapp;
 
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-public class AddRecepeActivity extends AppCompatActivity {
+public class AddRecepeActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Toolbar toolbar;
-
+    private ImageView ingrButton;
+    int ingrNr = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,11 +25,46 @@ public class AddRecepeActivity extends AppCompatActivity {
         this.setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        ingrButton = (ImageView) findViewById(R.id.addMorIngrButton);
+        ingrButton.setOnClickListener(this);
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.addMorIngrButton:
+                TextInputEditText editText = new TextInputEditText(this);
+                LinearLayout ingrLayout = (LinearLayout) findViewById(R.id.ingrLayoutID);
+
+                TextInputLayout textInputLayout = new TextInputLayout(this);
+                LinearLayout.LayoutParams textInputLayoutParams = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+
+                textInputLayout.setLayoutParams(textInputLayoutParams);
+                textInputLayout.addView(editText);
+               // textInputLayout.setDefaultHintTextColor(R.color.colorAccent);
+                //textInputLayout.setHint("Ingrediens");
+                editText.setId(R.id.ingredientID + ingrNr);
+                editText.setMaxLines(1);
+                editText.setHint(R.string.addIngredient);
+                editText.setSingleLine(true);
+                editText.setHintTextColor(getResources().getColor(R.color.colorAccent));
+                editText.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+                ingrLayout.addView(textInputLayout);
+                ingrNr += 1;
+                //setContentView(textInputLayout);
+                Log.d("!!!", String.valueOf(editText.getId()));
+                break;
+        }
+
     }
 }
