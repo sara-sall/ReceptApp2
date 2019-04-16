fdeb68d8c7383554f5b6e9e12d64f3ccd720bd17
sara-sall <sarasall092@gmail.com>
2019-04-16 15:05:11 +0200

    RecepePreviewActivity created and linked

--- app/src/main/java/com/example/receptapp/AddRecepeActivity.java
+++ app/src/main/java/com/example/receptapp/AddRecepeActivity.java
@@ -1,5 +1,6 @@
 package com.example.receptapp;
 
+import android.content.Intent;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -7,14 +8,30 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import java.util.ArrayList;
+
 public class AddRecepeActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Toolbar toolbar;
+    private EditText recepeTitle;
+    private EditText recepeDesc;
+    private EditText recepeIngr;
+    private EditText recepeInst;
+    private CheckBox recepeCB;
     private ImageView ingrButton;
+
+    private EditText rIngr;
+
+    private ArrayList idList;
+
+    private Button previewBtn;
+
     int ingrNr = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +39,21 @@ public class AddRecepeActivity extends AppCompatActivity implements View.OnClick
         setContentView(R.layout.activity_add_recepe);
 
         toolbar = findViewById(R.id.toolbarID);
+        toolbar.setTitle("Nytt recept");
         this.setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        idList = new ArrayList();
+
+        recepeTitle = (EditText) findViewById(R.id.recepeTitleID);
+        recepeDesc = (EditText) findViewById(R.id.recepeDescID);
+        recepeIngr = (EditText) findViewById(R.id.ingredientID);
+        recepeInst = (EditText) findViewById(R.id.recepeInstrID);
+
+        previewBtn = (Button) findViewById(R.id.recepePreviewID);
+        previewBtn.setOnClickListener(this);
+
 
         ingrButton = (ImageView) findViewById(R.id.addMorIngrButton);
         ingrButton.setOnClickListener(this);
@@ -53,18 +82,70 @@ public class AddRecepeActivity extends AppCompatActivity implements View.OnClick
                 textInputLayout.addView(editText);
                // textInputLayout.setDefaultHintTextColor(R.color.colorAccent);
                 //textInputLayout.setHint("Ingrediens");
-                editText.setId(R.id.ingredientID + ingrNr);
+                editText.setId(100 + ingrNr);
                 editText.setMaxLines(1);
                 editText.setHint(R.string.addIngredient);
                 editText.setSingleLine(true);
                 editText.setHintTextColor(getResources().getColor(R.color.colorAccent));
                 editText.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
                 ingrLayout.addView(textInputLayout);
+
+                idList.add(editText.getId());
                 ingrNr += 1;
                 //setContentView(textInputLayout);
                 Log.d("!!!", String.valueOf(editText.getId()));
                 break;
+
+            case R.id.recepePreviewID:
+                String title = recepeTitle.getText().toString().trim();
+                String desc = recepeDesc.getText().toString().trim();
+                String ingr = recepeIngr.getText().toString().trim();
+                String inst = recepeInst.getText().toString().trim();
+                ArrayList<String> ingrList = new ArrayList();
+                ingrList.add(ingr);
+
+                for(int i = 0; i<ingrNr; i++){
+                    String ID = String.valueOf(idList.get(i));
+                    rIngr = (EditText)findViewById(Integer.parseInt(ID));
+                    String ingredient = rIngr.getText().toString().trim();
+                    if(!ingredient.equals("")){
+                        ingrList.add(ingredient);
+                    }
+                }
+
+                if(title.isEmpty()){
+                    recepeTitle.setError("Recepttitel behöver fyllas i");
+                    recepeTitle.requestFocus();
+                    return;
+                }
+                if(desc.isEmpty()){
+                    recepeDesc.setError("Specialkost behöver fyllas i");
+                    recepeDesc.requestFocus();
+                    return;
+                }
+                if(ingr.isEmpty()){
+                    recepeIngr = (EditText) findViewById(R.id.ingredientID);
+                    recepeIngr.setError("Recepttitel behöver fyllas i");
+                    recepeIngr.requestFocus();
+                    return;
+                }
+                if(inst.isEmpty()){
+                    recepeInst.setError("Instruktioner behöver fyllas i");
+                    recepeInst.requestFocus();
+                    return;
+                }
+
+                Intent intent = new Intent(AddRecepeActivity.this, RecepePreviewActivity.class);
+
+                intent.putExtra("title", title);
+                intent.putExtra("desc", desc);
+                intent.putExtra("ingrList", ingrList);
+                intent.putExtra("inst", inst);
+
+                startActivity(intent);
         }
+
+
 
     }
 }
---
+++ app/src/main/java/com/example/receptapp/RecepePreviewActivity.java
@@ -0,0 +1,135 @@
+package com.example.receptapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class RecepePreviewActivity extends AppCompatActivity {
+
+    private TextView descText;
+    private TextView instText;
+    private TextView ingrList;
+    boolean isFavorite = true;
+    Toolbar toolbar;
+    ImageView rImage;
+    private String recepeTitle;
+    private String recepeDescription;
+    private String recepeInstructions;
+    private ArrayList ingridientList;
+    private Context context;
+
+    private FloatingActionButton fab;
+
+    private FirebaseFirestore db;
+    private DocumentReference receptRef;
+    private CollectionReference favoriteRef;
+
+    private FirebaseAuth mAuth;
+    public String user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recepe_preview);
+
+        descText = (TextView) findViewById(R.id.recepieADescID);
+        toolbar = (Toolbar) findViewById(R.id.rToolbar);
+        rImage = (ImageView) findViewById(R.id.rImage);
+        ingrList = (TextView) findViewById(R.id.ingredientsID);
+        instText = (TextView) findViewById(R.id.instructionsID);
+        toolbar.setTitle("");
+        context = getApplicationContext();
+
+        ingridientList = new ArrayList<String>();
+
+        Bundle b = new Bundle();
+        b = getIntent().getExtras();
+
+        if(b != null){
+            recepeTitle = (String) b.get("title");
+            recepeDescription = (String) b.get("desc");
+            recepeInstructions = (String) b.get("inst");
+            ingridientList = (ArrayList) b.get("ingrList");
+
+            descText.setText(recepeDescription);
+            // rImage.setImageResource(recept.getImage());
+            toolbar.setTitle(recepeTitle);
+            instText.setText(recepeInstructions);
+
+            String ingredients = "";
+            for(int i = 0; i < ingridientList.size(); i++){
+                String ingredient = ingridientList.get(i).toString();
+                ingredients = ingredients + "● " + ingredient + "\n";
+            }
+            ingrList.setText(ingredients);
+
+        }
+
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isFavorite){
+                    fab.setImageResource(R.drawable.ic_favorite_white_24dp);
+                    isFavorite = true;
+
+                }else{
+                    fab.setImageResource(R.drawable.ic_favorite_border_white_24dp);
+                    isFavorite = false;
+                }
+                fab.hide();
+                fab.show();
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+}
